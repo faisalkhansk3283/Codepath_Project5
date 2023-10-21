23c3839549e9774ba1b7fc1c5e2d39788f4083ba
Faisal Rasheed Khan <vb02734@umbc.edu>
2023-10-21 15:13:23 -0400

    Code for project 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,21 @@
+
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
   const [data, setData] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
-  const [temperatureFilter, setTemperatureFilter] = useState('');
   const [filteredData, setFilteredData] = useState([]);
-  const [totalItems, setTotalItems] = useState(0); // Add state for totalItems
-  const [meanTemperature, setMeanTemperature] = useState(0);
+  const [childFriendlyFilter, setChildFriendlyFilter] = useState(0);
+  const [sheddingLevelFilter, setSheddingLevelFilter] = useState('Any');
 
   useEffect(() => {
     async function fetchData() {
       try {
-        const response = await fetch('https://api.weatherbit.io/v2.0/current?lat=35.7796&lon=-78.6382&key=00d364ab88d647a7b3ab7a5bba620422');
+        const response = await fetch('https://api.thecatapi.com/v1/breeds?limit=30');
         const result = await response.json();
-        setData(result.data);
-        setFilteredData(result.data);
-        setTotalItems(result.data.length); // Set the total number of items
-        setMeanTemperature(
-          (
-            result.data.reduce((sum, item) => sum + item.temp, 0) / result.data.length
-          ).toFixed(2)
-        );
+        setData(result);
+        setFilteredData(result);
       } catch (error) {
         console.error('Error fetching data:', error);
       }
@@ -33,56 +27,145 @@ function App() {
   const handleSearch = (event) => {
     const searchTerm = event.target.value;
     setSearchTerm(searchTerm);
-    const filteredData = data.filter((item) =>
-      item.city_name.toLowerCase().includes(searchTerm.toLowerCase())
-    );
-    setFilteredData(filteredData);
+    filterData(searchTerm, childFriendlyFilter, sheddingLevelFilter);
   }
 
-  const handleTemperatureFilter = (event) => {
-    const selectedTemperature = event.target.value;
-    setTemperatureFilter(selectedTemperature);
-    const filteredData = data.filter((item) => {
-      if (selectedTemperature === 'cold') {
-        return item.temp < 10;
-      } else if (selectedTemperature === 'warm') {
-        return item.temp >= 10;
-      }
-      return true;
-    });
+  const handleChildFriendlyFilter = (value) => {
+    setChildFriendlyFilter(value);
+    filterData(searchTerm, value, sheddingLevelFilter);
+  }
+
+  const handleSheddingLevelFilter = (value) => {
+    setSheddingLevelFilter(value);
+    filterData(searchTerm, childFriendlyFilter, value);
+  }
+
+  const filterData = (search, childFriendly, sheddingLevel) => {
+    const filteredData = data
+      .filter((item) => (
+        item.name.toLowerCase().includes(search.toLowerCase()) &&
+        item.child_friendly >= childFriendly &&
+        (sheddingLevel === 'Any' || item.shedding_level.toString() === sheddingLevel)
+      ));
+    
     setFilteredData(filteredData);
   }
+  const calculateMean = (attribute) => {
+    const total = data.reduce((acc, item) => acc + item[attribute], 0);
+    return (total / data.length).toFixed(2);
+  }
+
+  const calculateQuartiles = (attribute) => {
+    const filteredData = data.filter((item) => item[attribute] !== undefined);
+  
+    if (filteredData.length === 0) {
+      // If there is no valid data for the given attribute, return placeholders.
+      return ['N/A', 'N/A', 'N/A'];
+    }
+  
+    const sortedData = [...filteredData].sort((a, b) => a[attribute] - b[attribute]);
+    const mid = Math.floor(sortedData.length / 2);
+  
+    const lowerHalf = mid % 2 === 0
+      ? sortedData.slice(0, mid)
+      : sortedData.slice(0, mid + 1);
+  
+    const upperHalf = sortedData.slice(mid);
+  
+    const lowerQuartile = lowerHalf[Math.floor(lowerHalf.length / 2)][attribute];
+    const median = mid % 2 === 0
+      ? (lowerHalf[mid - 1][attribute] + upperHalf[0][attribute]) / 2
+      : upperHalf[0][attribute];
+  
+    const upperQuartile = upperHalf[Math.floor(upperHalf.length / 2)][attribute];
+  
+    return [lowerQuartile, median, upperQuartile];
+  }
+  
+
+  const totalItems = data.length;
+  const meanAffectionLevel = calculateMean('affection_level');
+  const meanStrangerFriendly = calculateMean('stranger_friendly');
+  const meanChildFriendly = calculateMean('child_friendly');
+
+  const [affectionQuartiles, setAffectionQuartiles] = useState([]);
+  const [strangerFriendlyQuartiles, setStrangerFriendlyQuartiles] = useState([]);
+  const [childFriendlyQuartiles, setChildFriendlyQuartiles] = useState([]);
+
+  useEffect(() => {
+    const affectionQuartiles = calculateQuartiles('affection_level');
+    const strangerFriendlyQuartiles = calculateQuartiles('stranger_friendly');
+    const childFriendlyQuartiles = calculateQuartiles('child_friendly');
+  
+    setAffectionQuartiles(affectionQuartiles);
+    setStrangerFriendlyQuartiles(strangerFriendlyQuartiles);
+    setChildFriendlyQuartiles(childFriendlyQuartiles);
+  }, [data]);
+
+  
 
   return (
     <div className="App">
-      <h1>Data Dashboard</h1>
+      <h1>Cat Breed Dashboard</h1>
 
       <div className="summary">
         <p>Total Items: {totalItems}</p>
-        <p>Mean Temperature: {meanTemperature}°C</p>
+        <p>Mean Affection Level: {meanAffectionLevel}</p>
+        <p>Mean Stranger-Friendly Level: {meanStrangerFriendly}</p>
+        <p>Mean Child-Friendly Level: {meanChildFriendly}</p>
       </div>
 
-      <input
-        type="text"
-        placeholder="Search..."
-        value={searchTerm}
-        onChange={handleSearch}
-      />
+      <div className="quartiles">
+        <p>Affection Level Quartiles: Q1 = {affectionQuartiles[0]}, Q2 (Median) = {affectionQuartiles[1]}, Q3 = {affectionQuartiles[2]}</p>
+        <p>Stranger-Friendly Level Quartiles: Q1 = {strangerFriendlyQuartiles[0]}, Q2 (Median) = {strangerFriendlyQuartiles[1]}, Q3 = {strangerFriendlyQuartiles[2]}</p>
+        <p>Child-Friendly Level Quartiles: Q1 = {childFriendlyQuartiles[0]}, Q2 (Median) = {childFriendlyQuartiles[1]}, Q3 = {childFriendlyQuartiles[2]}</p>
+      </div>
 
-      <select value={temperatureFilter} onChange={handleTemperatureFilter}>
-        <option value="">Select Temperature</option>
-        <option value="cold">Cold</option>
-        <option value="warm">Warm</option>
-      </select>
-
-      <div className="data-list">
+      <div className="filters">
+        <input
+          type="text"
+          placeholder="Search Cat Names..."
+          value={searchTerm}
+          onChange={handleSearch}
+        />
+        <div className="filter">
+          <label>Child Friendly: </label>
+          <select
+            value={childFriendlyFilter}
+            onChange={(e) => handleChildFriendlyFilter(e.target.value)}
+          >
+            <option value={0}>Any</option>
+            <option value={4}>4 and above</option>
+          </select>
+        </div>
+        <div className="filter">
+          <label>Shedding Level: </label>
+          <select
+            value={sheddingLevelFilter}
+            onChange={(e) => handleSheddingLevelFilter(e.target.value)}
+          >
+            <option value="Any">Any</option>
+            <option value="1">Low</option>
+            <option value="2">Moderate</option>
+            <option value="3">High</option>
+            <option value="4">Very High</option>
+          </select>
+        </div>
+      </div>
+      
+<h1>Cat Names</h1>
+      <table className="data-list">
+      <thead>
+        
+      </thead>
+      <tbody>
         {filteredData.map((item) => (
-          <div key={item.city_name}>
-            <h2>{item.city_name}</h2>
-            <p>Temperature: {item.temp}°C</p>
-          </div>
+          <tr key={item.id}>
+            <td>{item.name}</td>
+          </tr>
         ))}
-      </div>
+      </tbody>
+    </table>
     </div>
   );
 }
